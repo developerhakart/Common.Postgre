4d385957b3632af6b9cb0a062920c70bb0f6f6d7
Arthur <developerhakart@yahoo.com>
2019-08-22 08:58:57 +0400

    initial postgre extension step

---
+++ Common.Postgre/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Common.Postgre
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddPostgre<T>(this IServiceCollection services)
+             where T : DbContext
+        {
+            PostgreOptions options = null;
+            using (var serviceProvider = services.BuildServiceProvider())
+            {
+                var configuration = serviceProvider.GetService<IConfiguration>();
+                services.Configure<PostgreOptions>(configuration.GetSection("postgre"));
+                options = configuration.GetOptions<PostgreOptions>("postgre");
+            }
+
+            if (!options.Enabled)
+            {
+                return services;
+            }
+
+            return services.AddEntityFrameworkNpgsql()
+                    .AddDbContext<T>(op => op.UseNpgsql(options.ConnectionString)); ;
+        }
+    }
+}
